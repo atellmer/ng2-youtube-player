17d1dd63ed1493d5e759f4751f8f6a5b4e3c4264
Alex Plex <atellmer@gmail.com>
2016-07-30 13:54:57 +0500

    small fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent, environment } from './app/';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { provideForms } from '@angular/forms';
+
+import { AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
